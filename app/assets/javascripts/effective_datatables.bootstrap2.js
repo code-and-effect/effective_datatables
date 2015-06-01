43d4d32d4794d888951edb362dc4fc074aa3e65e
Matt Riemer <matthew@agilestyle.com>
2015-06-01 09:56:17 -0600

    Remove scroller, disable fixedcolumn

--- app/assets/javascripts/effective_datatables.bootstrap2.js
+++ app/assets/javascripts/effective_datatables.bootstrap2.js
@@ -2,12 +2,11 @@
 //= require dataTables/bootstrap/2/jquery.dataTables.bootstrap
 //= require dataTables/dataTables.colReorder.min
 //= require dataTables/dataTables.colVis.min
-//= require dataTables/dataTables.fixedColumns
 //= require dataTables/dataTables.tableTools.min
 //= require vendor/jquery.debounce.min
 
 //= require_tree ./effective_datatables
 
 $.extend( $.fn.dataTable.defaults, {
-  'dom': "<'row'<'span6'><'span6'TCl>r><'scroll_wrapper't><'row'<'span6'i><'span6'p>>"
+  'dom': "<'row header-row'<'span4'l><'span6'TC>r><'scroll_wrapper't><'row'<'span6'i><'span6'p>>"
 });
--- app/assets/javascripts/effective_datatables.js
+++ app/assets/javascripts/effective_datatables.js
@@ -2,13 +2,12 @@
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require dataTables/dataTables.colReorder.min
 //= require dataTables/dataTables.colVis.min
-//= require dataTables/dataTables.fixedColumns.min
 //= require dataTables/dataTables.tableTools.min
 //= require vendor/jquery.debounce.min
 
 //= require_tree ./effective_datatables
 
 $.extend( $.fn.dataTable.defaults, {
-  'dom': "<'row header-row'<'col-sm-4'><'col-sm-8'TCl>r><'scroll_wrapper't><'row'<'col-md-6'i><'col-md-6'p>>"
+  'dom': "<'row header-row'<'col-sm-4'l><'col-sm-8'TC>r><'scroll_wrapper't><'row'<'col-md-6'i><'col-md-6'p>>"
 });
 
