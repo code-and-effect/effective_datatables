37007bc8f3f7342bc7353497c997ab0f4b13c1a7
Matt Riemer <matthew@agilestyle.com>
2015-12-02 14:00:49 -0700

    Buttons working properly

--- app/assets/javascripts/effective_datatables.js
+++ app/assets/javascripts/effective_datatables.js
@@ -1,4 +1,17 @@
 //= require vendor/jquery.debounce.min
+//= require vendor/jszip.min
+
 //= require dataTables/jquery.dataTables.min
 //= require dataTables/jquery.dataTables.bootstrap
+
+//= require dataTables/buttons/dataTables.buttons
+//= require dataTables/buttons/buttons.bootstrap
+//= require dataTables/buttons/buttons.colVis
+//= require dataTables/buttons/buttons.html5
+//= require dataTables/buttons/buttons.print
+
 //= require_tree ./effective_datatables
+
+$.extend( $.fn.dataTable.defaults, {
+  'dom': "<'row'<'col-sm-6'l><'col-sm-6'B>><'row'<'col-sm-12'tr>><'row'<'col-sm-5'i><'col-sm-7'p>>"
+});
