cb599f1e5e27d8a7577966f293980f60247517e0
Matt Riemer <matthew@agilestyle.com>
2016-09-14 13:47:17 -0600

    array_columns should never be marked sql_as_column

--- app/models/effective/effective_datatable/options.rb
+++ app/models/effective/effective_datatable/options.rb
@@ -146,7 +146,7 @@ module Effective
             cols[name][:column] = sql_table.present? ? "#{quote_sql(sql_table.name)}.#{quote_sql('roles_mask')}" : name
           end
 
-          if sql_table.present? && sql_column.blank? # This is a SELECT AS column, or a JOIN column
+          if sql_table.present? && sql_column.blank? && !cols[name][:array_column] # This is a SELECT AS column, or a JOIN column
             cols[name][:sql_as_column] = true
           end
 
