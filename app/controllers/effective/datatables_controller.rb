030f63e8c30186057f0b1cc9c80f706e84cd42b1
Matt Riemer <matthew@agilestyle.com>
2017-07-14 12:23:03 -0600

    better error handling

--- app/controllers/effective/datatables_controller.rb
+++ app/controllers/effective/datatables_controller.rb
@@ -14,7 +14,8 @@ module Effective
       rescue => e
         (EffectiveDatatables.authorized?(self, :index, @datatable.try(:collection_class)) rescue false)
 
-        render json: error_json(message: e.message)
+        render json: error_json(e)
+        raise e
       end
     end
 
@@ -25,11 +26,11 @@ module Effective
       id.classify.safe_constantize || id.classify.pluralize.safe_constantize
     end
 
-    def error_json(message:)
+    def error_json(e)
       {
         data: [],
         draw: params[:draw].to_i,
-        effective_datatables_error: message.presence || 'unknown error',
+        effective_datatables_error: (e.message.presence unless e.class.name.include?('ActiveRecord::')) || 'unexpected operation',
         recordsTotal: 0,
         recordsFiltered: 0,
         aggregates: [],
