12e9fe3436013b7f3801b2f6ab210ca980d2f2cc
Matt Riemer <matthew@agilestyle.com>
2018-03-21 10:06:39 -0600

    better cookie_param

--- app/controllers/effective/datatables_controller.rb
+++ app/controllers/effective/datatables_controller.rb
@@ -24,7 +24,7 @@ module Effective
     private
 
     def find_datatable(id)
-      id = id.to_s.gsub(/--?\d+\z/, '')
+      id = id.to_s.gsub(/-\d+\z/, '').gsub('-', '/')
       id.classify.safe_constantize || id.classify.pluralize.safe_constantize
     end
 
