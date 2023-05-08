45fe9ddb74b1fd82b414e7bb8972891b02362fcf
Matt Riemer <matthew@codeandeffect.com>
2023-05-08 10:26:40 -0600

    updates for rails 7+

--- lib/effective_datatables/engine.rb
+++ lib/effective_datatables/engine.rb
@@ -1,16 +1,26 @@
+require_relative '../../app/helpers/effective_datatables_controller_helper'
+require_relative '../../app/helpers/effective_datatables_helper'
+require_relative '../../app/helpers/effective_datatables_private_helper'
+
 module EffectiveDatatables
   class Engine < ::Rails::Engine
     engine_name 'effective_datatables'
 
     config.autoload_paths += Dir["#{config.root}/app/models/concerns", '/app/datatables/**/']
 
+    initializer 'effective_datatables.assets' do |app|
+      app.config.assets.precompile += ['effective_datatables_manifest.js', 'images/*']
+    end
+
     # Include Helpers to base application
     initializer 'effective_datatables.action_controller' do |app|
-      ActiveSupport.on_load :action_controller_base do
-        helper EffectiveDatatablesHelper
-        helper EffectiveDatatablesPrivateHelper
+      app.config.to_prepare do
+        ActiveSupport.on_load :action_controller_base do
+          helper EffectiveDatatablesHelper
+          helper EffectiveDatatablesPrivateHelper
 
-        ActionController::Base.send :include, ::EffectiveDatatablesControllerHelper
+          ActionController::Base.send :include, ::EffectiveDatatablesControllerHelper
+        end
       end
     end
 
